d38ab070eded0f67c7404c9e23b47c605dd35691
Rishabh Sharma <154492131+RishabhDecrypt@users.noreply.github.com>
2023-12-27 10:56:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const twilio = require("twilio");
 const jwt = require('jsonwebtoken');
